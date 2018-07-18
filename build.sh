89dcde4474a7fe8a1afd0d5dbacfc07be8ecd175
Steven Cook <scook@velocity.org>
2018-07-18 16:22:26 -0400

    Tags are now in separate directories. Build file updated to use -t option to specify a tag now.

--- build.sh
+++ build.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
 
-docker build --compress -t velocityorg/docker-gitlab-runner:latest .
+function usage() {
+    echo -e "Syntax: $(basename $0) -t <tag>"
+    echo -e "See the tags/ directory for a list of valid tags"
+}
+
+while getopts :t: opt; do
+    case "$opt" in
+        t)
+            tag=${OPTARG}
+            ;;
+        :)
+            usage; echo
+            >&2 echo "Option -$OPTARG requires an argument"
+            exit 1
+            ;;
+        ?)
+            usage; echo
+            >&2 echo "Invalid option -$OPTARG"
+            exit 1
+            ;;
+        *)
+            usage; echo
+            >&2 echo "Undefined option -$OPTARG"
+            exit 1
+            ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+if [[ -n ${tag} ]]; then
+    if [[ -d "tags/${tag}" ]]; then
+        docker build --compress -t "velocityorg/docker-gitlab-runner:${tag}" "tags/${tag}/"
+    else
+        usage; echo
+        >&2 echo "\"${tag}\" isn't a valid tag"
+        exit 1
+    fi
+else
+    usage; exit 1
+fi
+
+exit 0
